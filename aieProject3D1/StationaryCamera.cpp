ef5d266a31da6835556cdcdf6021ed39986f87e9
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2023-03-20 19:14:49 +1100

    Finished stationary camera

--- aieProject3D1/StationaryCamera.cpp
+++ aieProject3D1/StationaryCamera.cpp
@@ -2,8 +2,6 @@
 
 void StationaryCamera::SetRotation(glm::vec3 rotation)
 {
-	float angle = glm::length(rotation);
-	glm::vec3 normal = glm::normalize(rotation);
-
-	glm::rotate(m_worldTransform, angle, normal);
+	m_theta = rotation.x;
+	m_phi = rotation.y;
 }
